e6ba9c8b6ed06ee8ee21a7dc39366fdadead442d
pradhuman <praduman1997@gmail.com>
2021-07-24 16:56:56 +1000

    Redirecting of the pages.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import Login from "./components/Login";
+import Dashboard from "./components/Dashboard"
 import "bootstrap/dist/css/bootstrap.min.css"
 import { UserProvider } from "./components/UserContext";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
-  return (
-    <UserProvider>
-      <div className="App">
-        <Login />
-      </div>
-    </UserProvider>
-    
-  );
-}   
+    return (
+        <Router>
+            <UserProvider>
+                <div className="App">
+                    <Switch>
+                        <Route exact path="/">
+                            <Login />
+                        </Route>
+                        <Route path="/dashboard">
+                            <Dashboard/>
+                        </Route>
+                    </Switch>
+                </div>
+            </UserProvider>
+        </Router>
+    );
+}
 
 export default App;
---
+++ src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Button, Card, Form} from "react-bootstrap";
+import App from "../App";
+
+function Dashboard()
+{
+
+
+    return (
+        <p> Hi i am in dashboard</p>
+
+    )
+}
+
+export default Dashboard;
